348d91797fe2e137f9b64a4f3e9264d846691856
ksdueki <ueki@aok-net.com>
2020-01-23 18:25:16 +0900

    追加

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rails_template
+* 参考：[rails6\_template/app\_template\.rb at master · Iwark/rails6\_template](https://github.com/Iwark/rails6_template/blob/master/app_template.rb)
+
+## Usage
+
+```console
+$ rails new app -d postgresql -BTC --skip-turbolinks -m app_template.rb
+```
+
+## Motivation
+
+- newのあとにGemfileを編集したくない
+- bundleのあとの必須設定を自動でやってほしい
+  - i18nのja.ymlのダウンロード
+  - RSpec generate時の作成ファイルの設定
+  - Gemfileのコメント行の削除
+  - 
--- app_template.rb
+++ app_template.rb
@@ -1,10 +1,46 @@
+#
+# hirocueki-rails-template
+#
+# Usage:
+#   rails new appname -d postgresql -m /path/to/app_template.rb
+#
+
+def get_remote(src, dest = nil)
+  dest ||= src
+  repo = 'https://raw.github.com/hirocueki/rails_template/master/files/'
+  remote_file = repo + src
+  remove_file dest
+  get(remote_file, dest)
+end
+
+
+# Check prerequisites
+%w{colored}.each do |component|
+  run "gem install #{component}"
+  Gem.refresh
+end
+
+require "colored"
+
+puts "\n========================================================="
+puts " HIROCUEKI RAILS 6 TEMPLATE".yellow.bold
+puts "=========================================================\n"
+
 gem 'hamlit-rails'
 gem 'rails-i18n'
 
+# option: ユーザー認証にdeviseを使うか
 if yes?('devise?')
   gem 'devise'
   gem 'devise-i18n'
   gem 'devise-i18n-views'
+end
+
+if yes?('dotenv-rails?')
+  gem_group :development do
+    gem 'dotenv-rails'
+  end
+  run 'touch .env'
 end
 
 gem_group :development, :test do
@@ -19,4 +55,35 @@ gem_group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+end
+
+
+# 
+after_bundle do
+
+  # Devise
+  generate 'devise:install'
+  get_remote('config/locales/devise.en.yml')
+  get_remote('config/locales/devise.ja.yml')
+
+  rails "db:create db:migrate"
+  git add: '.'
+  git commit: "-am 'migrate database'"
+  
+  puts <<-CODE
+    # ADD TO config/application.rb
+    config.time_zone = 'Tokyo'
+
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.factory_bot false
+      g.factory_bot dir: 'spec/factories'
+
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_spec: false,
+        routing_specs: false
+    end
+  CODE
 end
